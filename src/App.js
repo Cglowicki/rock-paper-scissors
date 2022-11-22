bec777a3ed174e9f3c86a0b25be37a8040e3fa80
Cameron Reed Glowicki <crg@Camerons-MacBook-Air.local>
2022-11-21 18:15:12 -0800

    add button component, import to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Button from './components/Button'
 
 function App() {
   return (
     <div className="App">
       ROCK, PAPER, SCISSORS!
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Button() {
+  return (
+    <button>CLICK</button>
+  );
+}
+
+export default Button;
